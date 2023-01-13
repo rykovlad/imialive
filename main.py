8951af67b538b72caf44e94b16fda1f35421ccfa
vlad.rikov.2002 <vlad.rikov.2002@gmail.com>
2023-01-13 17:11:35 +0200

    add start and end of work in main channel

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pprint
 import time
 import aioschedule
 
+from datetime import datetime
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import ContentType
 from aiogram.utils import executor
@@ -24,16 +26,6 @@ async def process_is_bot_alive(message: types.Message):
     await bot.send_message(message.chat.id, "так, " + message.from_user.first_name + ", світло і інет є. не спам пліз")
 
 
-@dp.message_handler(commands=['update_bot_on_server'])
-async def process_update_bot_on_server(message: types.Message):
-    if message.from_id in config.BOSSES_ID:
-        import subprocess
-        subprocess.call(config.PATH_TO_UPDATER_SCRIPT)
-        await message.reply("done)")
-    else:
-        await message.reply("fuck oF")
-
-
 @dp.message_handler(content_types=ContentType.ANY)
 async def unknown_message():
     pass
@@ -41,28 +33,26 @@ async def unknown_message():
 
 async def scheduler():
     await send_4_all()
-    aioschedule.every(3).minutes.do(send_4_all)
+    aioschedule.every(2).minutes.do(send_4_all)
     while True:
         await aioschedule.run_pending()
         await asyncio.sleep(1)
 
 
 async def send_4_all():
-    if mode == "release":
-        try:
-            await bot.send_message(config.CHANNEL_ID_LOGS, config.MSG_LOGS)
-            ob.new_state = 1
-        except NetworkError as e:
-            print("noooo \n", e)
-            ob.new_state = 0
-
-        if (ob.state != ob.new_state) & (ob.new_state == 1):
+    if mode == "release" and ob.state:
+        if ob.is_first:
+            ob.m = await bot.send_message(config.CHANNEL_ID, config.MSG_LIGHT_BACK)
             await bot.send_message(config.CHANNEL_ID_LOGS, config.MSG_LIGHT_BACK)
-            time.sleep(1)
-            await bot.send_message(config.CHANNEL_ID, config.MSG_LIGHT_BACK)
-        elif ob.new_state == 0:
-            pass  # mb it will be msg when power off, but i`m not sure
-        ob.state = ob.new_state
+            ob.time_internet_start = datetime.now().strftime("%d/%m/%Y, %H:%M")
+            ob.is_first = 0
+        else:
+            text = config.MSG_LIGHT_BACK
+            text = text + "\nсвітло вернулось в " + time_power_start
+            text = text + "\nінет вернувся    в " + ob.time_internet_start
+            text = text + "\nбули востаннє    в " + datetime.now().strftime("%d/%m/%Y, %H:%M")
+            await ob.m.edit_text(text)
+            await bot.send_message(config.CHANNEL_ID_LOGS, config.MSG_LOGS)
 
 
 async def on_startup(_):
@@ -70,9 +60,10 @@ async def on_startup(_):
 
 
 if __name__ == '__main__':
+    time_power_start = datetime.now().strftime("%d/%m/%Y, %H:%M")
     while True:
         try:
             executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
         except NetworkError as e:
             print(e)
-        time.sleep(31)
+        time.sleep(15)
--- observer.py
+++ observer.py
@@ -1,11 +1,29 @@
+import subprocess
+
+
 class Observer:
     def __init__(self):
         self._state = 0
         self._new_state = 0
+        self._is_first = 1
+        self.time_internet_start = "nooo way pogarmist durenb"
+
+    @property
+    def is_first(self):
+        return self._is_first
+
+    @is_first.setter
+    def is_first(self, value):
+        self._is_first = value
+
 
     @property
     def state(self):
-        return self._state
+        res = subprocess.call(['ping', '-c', '1', "8.8.8.8"], stdout=subprocess.DEVNULL)
+        if res == 0:
+            return True
+        else:
+            return False
 
     @state.setter
     def state(self, value):
